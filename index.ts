21e56f67ed32f71fb7f3adfd5111e1dfba795136
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-09-23 00:31:46 +0530

    add basic entry point

--- index.ts
+++ index.ts
@@ -1,3 +1,37 @@
 #!/usr/bin/env bun
 
-console.log('Hello via Bun!');
+import { log } from './src/observability/logger';
+
+const EXIT = '/exit';
+const QUIT = '/quit';
+const ASK = '/ask ';
+
+const run = async () => {
+  log.info('Starting CodeMint...');
+  console.log(`Code Mint - RAG powered code assistant`);
+  console.log(`Type /exit to quit \n`);
+
+  while (true) {
+    const userInput = prompt('>')?.trim();
+    if (!userInput) {
+      continue;
+    }
+    if (userInput.toLowerCase() === EXIT || userInput.toLowerCase() === QUIT) {
+      log.info('Shutting down...');
+      console.log(`Goodbye!`);
+      break;
+    } else if (userInput.toLowerCase().startsWith(ASK)) {
+      const question = userInput.slice(ASK.length).trim();
+      log.info('Ask command received:', question);
+      console.log(`Searching for: ${question}...`);
+      // TODO : Call retriever + LLM
+    } else {
+      log.warn('Unknown command received:', userInput);
+      console.log(`Unknown command. Try:`);
+      console.log(`\task <question> - ask a question`);
+      console.log(`\task <question> - ask a question`);
+    }
+  }
+};
+
+await run();
